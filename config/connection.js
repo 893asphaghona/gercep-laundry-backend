60a6dbc00cd25a52575e47a72792f51ca91e4295
893asphaghona <ricardocandrasulebaganti@gmail.com>
2025-01-31 01:36:12 +0700

    Update .env and connection.js for database configuration

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
 });
 
