e736f444734591a81cd852160cb3a9a77966e0fc
893asphaghona <ricardocandrasulebaganti@gmail.com>
2025-02-03 02:17:09 +0700

    ayolah

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const sequelize = require('./config/connection'); // Koneksi Sequelize
 
 const app = express();
-const port = process.env.PORT || 5001;
+const port = 5001;
 
 // Middleware
 app.use(cors({
