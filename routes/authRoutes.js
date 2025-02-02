02544067e35ee24e12d22f6e06c69ddd4cbf4187
893asphaghona <ricardocandrasulebaganti@gmail.com>
2025-02-03 03:54:42 +0700

    weniwn

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { loginUser, registerUser } = require('../controllers/authController');
 
 // Rute login
-router.post('/api/login', loginUser);
+router.post('/login', loginUser);
 
 // Rute register
 router.post('/register', registerUser);
